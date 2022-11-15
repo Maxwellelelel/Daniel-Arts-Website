aad9370a634ef8431898cab6fa935e04824566a8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 11:07:09 +0100

    15-11-2022 (old)

--- JavaScript/style-script.js
+++ JavaScript/style-script.js
@@ -1,5 +1,5 @@
-var videoDiv = document.getElementById('video-container-id').style;
-var vid = document.getElementById('background-video');
+var videoDiv = document.getElementById('paravid-con').style;
+var vid = document.getElementById('bg-vid');
 var bds = document.body.style;
 
 function changeBackground(mediaURL) {
