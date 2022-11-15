fd75a906db6947f923f76f72fdab40ac66f4e43e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 12:47:35 +0100

    15-11-2022 (Rework)

--- JavaScript/style-script.js
+++ JavaScript/style-script.js
@@ -1,18 +1,60 @@
-var videoDiv = document.getElementById('paravid-con').style;
-var vid = document.getElementById('bg-vid');
-var bds = document.body.style;
+var bods = document.body.style;
 
-function changeBackground(mediaURL) {
-    var path = "url('" + mediaURL + "')";
+function changeSiteElements(changeAtt) {
+    var att = "" + changeAtt;
 
-    videoDiv.visibility = "hidden"
-    vid.pause();
-    bds.backgroundImage = path;
-    bds.transition = "0.8s";
+    switch (att) {
+        case "ve":
+            applyVideoEditingElements();
+            break;
+
+        case "sm":
+            applySocialMediaElements();
+            break;
+
+        case "dr":
+            applyDrawingElements();
+            break;
+        default:
+            console.log("Error: Internal error while applying page changes.");
+            console.log("Error: style-scripts.js: line: 18, line: 6, line: 3");
+    }
 }
 
-function toggleVideo() {
-    videoDiv.visibility = "visible";
-    vid.play();
-    bds.backgroundImage = "none"
+function applyDrawingElements() {
+    var bgPath = "url('Media/Images/Image7.png')"
+
+    bods.backgroundImage = bgPath;
+    bods.transition = "0.8s";
+
+    toggleVideo(false);
+}
+
+function applySocialMediaElements() {
+    var bgPath = "url('Media/Social/Third Frame.png')"
+
+    bods.backgroundImage = bgPath;
+    bods.transition = "0.8s";
+
+    toggleVideo(false);
+
+}
+
+function applyVideoEditingElements() {
+    toggleVideo(true);
+
+}
+
+function toggleVideo(isVideo) {
+    var videoDiv = document.getElementById('vidcon-id').style;
+    var vid = document.getElementById('bgvid-id');
+
+    if(isVideo) {
+        vid.play();
+        videoDiv.visibility = "visible";
+
+    } else {
+        vid.pause();
+        videoDiv.visibility = "hidden";
+    }
 }
